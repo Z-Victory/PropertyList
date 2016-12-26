8a034c5edc83a6208a2136b322882f6538c8a2b7
Z-Victory <310346124@qq.com>
2016-12-26 12:00:14 +0800

    Initial Commit

---
+++ 本地文件写入/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  本地文件写入
+//
+//  Created by IOS on 16/12/26.
+//  Copyright © 2016年 IOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 本地文件写入/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  本地文件写入
+//
+//  Created by IOS on 16/12/26.
+//  Copyright © 2016年 IOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
